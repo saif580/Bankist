58d697b170477f25a52021ce06d7ad319b63b91c
saif580 <saifulhasan580@gmail.com>
2023-07-03 22:14:48 +0530

    close account

--- script.js
+++ script.js
@@ -166,6 +166,20 @@ btnTransfer.addEventListener('click',function(e){
   }
 })
 
+btnClose.addEventListener('click',function(e){
+  e.preventDefault();
+  
+  const closeAccount=accounts.find(acc=>
+    acc.username===inputCloseUsername.value
+  )
+  if(inputCloseUsername.value===currentAccount.username && Number(inputClosePin.value)===currentAccount.pin){
+    const index=accounts.findIndex(acc=>acc.username===currentAccount.username);
+    accounts.splice(index,1);
+    containerApp.style.opacity=0;
+    inputCloseUsername.value=inputClosePin.value='';
+  }
+})
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
